aa87c4fd8ddcf043db782bb4791875c0baf4df1a
snaulX <alexandergunger666@gmail.com>
2020-01-29 12:12:38 +0300

    Add keywords to Parser and add reading file

--- src/mingwMain/kotlin/com/snaulX/Tangara/MainProgram.kt
+++ src/mingwMain/kotlin/com/snaulX/Tangara/MainProgram.kt
@@ -1,5 +1,9 @@
 package com.snaulX.Tangara
 
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.pointed
+import platform.posix.*
+
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("""
@@ -11,5 +15,13 @@ fun main(args: Array<String>) {
     }
     else {
         val parser: Parser = Parser()
+        val file: CPointer<FILE>? = fopen(args[0], "r")
+        var c: Int = getc(file)
+        val buffer: StringBuilder = StringBuilder()
+        while (c != EOF)
+        {
+            buffer.append(c.toChar())
+        }
+        parser.getCode(buffer.toString())
     }
 }
--- src/mingwMain/kotlin/com/snaulX/Tangara/Parser.kt
+++ src/mingwMain/kotlin/com/snaulX/Tangara/Parser.kt
@@ -1,5 +1,7 @@
 package com.snaulX.Tangara
 
+import platform.posix.fopen
+
 class Parser {
     var import_keyword = "import"
     var use_keyword = "use"
@@ -29,16 +31,29 @@ class Parser {
     var statement_end = ")"
     var block_start = "{"
     var block_end = "}"
+    var directive_start = "#"
+    var single_comment = "//"
+    var multiline_comment_start = "/*"
+    var multiline_comment_end = "*/"
+    var typeof_keyword = "typeof"
+    var is_keyword = "is"
+    var with_keyword = "with"
+    var switch_keyword = "switch"
+    var case_keyword = "case"
+    var default_keyword = "default"
+    var break_keyword = "break"
+    var continue_keyword = "continue"
 
     private var line = 0
     private var pos = 0
-    private var code: MutableList<String> = mutableListOf()
+    private var code: List<String> = listOf()
     val errors: MutableList<TangaraError> = mutableListOf()
     private var buffer: StringBuilder = StringBuilder()
     private val current: Char
         get() {
             return code[line][pos]
         }
+    var output: MutableList<Byte> = mutableListOf()
 
     fun skipWhitespaces(): Int {
         while (current.isWhitespace())
@@ -56,5 +71,13 @@ class Parser {
             }
         }
         return 0
+    }
+
+    fun getCode(input: String) {
+        code = input.split('\n')
+    }
+
+    fun buildTokens(tokens_name: String) {
+        val file = fopen(tokens_name, "wb")
     }
 }
