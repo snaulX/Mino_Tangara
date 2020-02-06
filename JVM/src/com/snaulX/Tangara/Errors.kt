130f2c255f7677a16520772f6acf8f27b8ab297f
snaulX <alexandergunger666@gmail.com>
2020-02-06 19:44:20 +0300

    Create JVM version

---
+++ JVM/src/com/snaulX/Tangara/Errors.kt
@@ -0,0 +1,11 @@
+package com.snaulX.Tangara
+
+fun TangaraError.getError() = "${this::class.simpleName} in line $line position $position. $message\n"
+
+class ImportError(line: Int, position: Int, message: String) : TangaraError(line, position, message)
+
+class UseError(line: Int, position: Int, message: String) : TangaraError(line, position, message)
+
+class LibError(line: Int, position: Int, message: String) : TangaraError(line, position, message)
+
+class SyntaxError(line: Int, position: Int, message: String) : TangaraError(line, position, message)
