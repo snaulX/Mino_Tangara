8878eda39f2c21eaf0128dca0683f11352f4b5ae
snaulX <alexandergunger666@gmail.com>
2020-02-17 19:50:32 +0300

    Create Dart project

---
+++ Dart/TangaraDart/src/main.dart
@@ -0,0 +1,13 @@
+import '../lib/Parser.dart' as Parser;
+
+void main(List<String> arguments) {
+    if (arguments.isEmpty) {
+        print("Tangara 2020-2020");
+        print("Author: snaulX");
+        print("License: MIT License");
+        print("All copyrights resreved");
+    }
+    else {
+        //pass
+    }
+}
